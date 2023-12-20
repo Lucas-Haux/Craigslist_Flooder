fe74d21d89f2793f32f4a0b77d8246516169d333
Lucas-Haux <60267132+Lucas-Haux@users.noreply.github.com>
2023-12-19 23:13:21 -0800

    more

--- Craigslist_Flooder/src/main.rs
+++ Craigslist_Flooder/src/main.rs
@@ -1,65 +1,95 @@
 use std::fs;
 use rand::prelude::*;
+use std::error::Error;
+use csv::{ReaderBuilder, Trim};
+use std::fmt::Display;
 
 fn main() {
-    let username = grab_date;
-    println!("username {}", username().to_string());
+
+   // _ran_hobbies();
+
+    println!("test: {}", _ran_hobbies());
+
+    /*
+    let username = _ran_name;
+    println!("username {}", username);
     let mut fake_email = String::new();
+
 
     fake_email.push_str("test");
     println!("fake_email: {}", fake_email);
+    */
 }
 
-fn _grab_name() {
+fn _ran_name() -> String {
+    let mut name = String::new();
     let file_path = "/home/luke/Rust/cli_programs/Craigslist_Flooder/Craigslist_Flooder/data/Popular_Baby_Names.csv";
 
     let name_list = match fs::read_to_string(file_path) {
         Ok(content) => content,
         Err(_) => {
             println!("Error reading the file '{}'", file_path);
-            return;
+            return String::new();
         }
     };
 
     // Split the string by commas
     let mut fields = name_list.split(',');
 
-        // Skip everything before the 3rd comma
-        for _ in 0..3 {
-            if let Some(_) = fields.next() {
-                // Continue skipping
-            } else {
-                // Handle case where there are not enough commas
-                println!("Not enough commas in the input string.");
-                return;
-            }
-        }
-
-        // Grab everything until the 4th comma
-        if let Some(result) = fields.next() {
-            println!("Result: {}", result);
+    let mut rng = thread_rng();
+    let num = rng.gen_range(0..57580) * 5;
+    // Skip lines
+    for _ in 0..num {
+        if let Some(_) = fields.next() {
+            // Continue skipping
         } else {
             // Handle case where there are not enough commas
             println!("Not enough commas in the input string.");
+            return String::new();
         }
+    }
 
-        for _ in 0..1 {
-            if let Some(_) = fields.next() {
-                // Continue skipping
-            } else {
-                // Handle case where there are not enough commas
-                println!("Not enough commas in the input string.");
-                return;
-            }
+    // Skip everything before the 3rd comma
+    for _ in 0..3 {
+        if let Some(_) = fields.next() {
+            // Continue skipping
+        } else {
+            // Handle case where there are not enough commas
+            println!("Not enough commas in the input string.");
+            return String::new();
         }
+    }
+
+    // Grab everything until the 4th comma
+    if let Some(temp_name) = fields.next() {
+        name = temp_name.to_string();
+        println!("Result: {}", name);
+    } else {
+        // Handle case where there are not enough commas
+        println!("Not enough commas in the input string.");
+    }
+
+    for _ in 0..1 {
+        if let Some(_) = fields.next() {
+            // Continue skipping
+        } else {
+            // Handle case where there are not enough commas
+            println!("Not enough commas in the input string.");
+            return String::new();
+        }
+    }
+    name
 }
 
-fn grab_date() -> i32 {
+fn _ran_date() -> String {
     let date: i32;
+
     let mut rng = thread_rng();
+
     let millennium: i32;
-    let coinflip = rng.gen();
     let decade: i32;
+
+    let coinflip = rng.gen();
     match coinflip {
         true => 
             millennium = 1900,
@@ -68,11 +98,43 @@ fn grab_date() -> i32 {
     }
     match coinflip {
         true => 
-            decade = rng.gen_range(70..100),
+            decade = rng.gen_range(70..=99),
         false => 
-            decade = rng.gen_range(0..24),
+            decade = rng.gen_range(0..=23),
     }
+
     date = millennium + decade;
     println!("date: {}", date);
-    date
+    date.to_string()
 }
+
+fn _ran_domain() -> String {
+    let mut domain = String::new();
+    let mut rng = thread_rng();
+
+    let random: u32 = rng.gen_range(0..=100);
+    match random {
+        0..=7 => domain = String::from("@comcast.net"), // 7%
+        8..=17 => domain = String::from("@aol.com"), // 11%
+        18..=63 => domain = String::from("@gmail.com"), // 47% %
+        64..=86 => domain = String::from("@yahoo.com"), // 24%%
+        87..=100 => domain = String::from("@outlook.com"), // 10%
+        101_u32..=u32::MAX => panic!(),
+    }
+    domain
+}
+
+fn _ran_hobbies() -> String {
+
+    let rdr = ReaderBuilder::new()
+    .from_path("/home/luke/Rust/cli_programs/Craigslist_Flooder/Craigslist_Flooder/data/hobbies.csv");
+    if let Some(result) = rdr.expect("REASON").records().choose(&mut rand::thread_rng()) {
+        let message = result.as_ref().expect("REASON").as_slice().to_owned();
+        println!("{}", message);
+        message
+    } else {
+        "No hobbies found".to_owned()
+    }
+    //println!("hobbie: {}", hobbie);
+}
+
---
+++ Craigslist_Flooder/target/debug/deps/Craigslist_Flooder-03ad26aa1e46479c.d
@@ -0,0 +1,5 @@
+/home/luke/Rust/cli_programs/Craigslist_Flooder/Craigslist_Flooder/target/debug/deps/Craigslist_Flooder-03ad26aa1e46479c: src/main.rs
+
+/home/luke/Rust/cli_programs/Craigslist_Flooder/Craigslist_Flooder/target/debug/deps/Craigslist_Flooder-03ad26aa1e46479c.d: src/main.rs
+
+src/main.rs:
